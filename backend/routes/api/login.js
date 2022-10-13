0d15c0bc481f38229f7a09175ec211690eb3c2f0
LantanaHP <115255396+LantanaHP@users.noreply.github.com>
2022-10-13 14:07:02 +0100

    Basic express setup
    Slight rearrangement of file structure, and basic routing logic has been put in place - in need of further development as content is added.
    
    Also, remember to build static pages with Vue commands, and to actually run the Node server.

---
+++ backend/routes/api/login.js
@@ -0,0 +1,25 @@
+//
+//INCLUDES AND HANDLES
+//
+
+//Includes
+const express = require("express");
+//const mongodb = require("mongodb"); //For when Mongo gets added in
+
+//Views path handle
+const path = __dirname + '../../../build';
+
+//Router handle
+const router = express.Router();
+
+//
+//ROUTING LOGIC
+//
+
+//Note that because this file is used through an app.x command, the '/' below expands to '/login/'
+router.get('/', (request, response) =>{
+    response.sendFile(path + "index.html");
+})
+
+
+module.exports = router;
