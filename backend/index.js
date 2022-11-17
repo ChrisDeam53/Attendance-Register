6824f9786b66451881e19fab457dbd10bdeb1c22
Mazen Omar <b9042121@my.shu.ac.uk>
2022-11-17 07:56:27 +0000

    Adjusted some of the relationships and added a bit of dummy data code

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require("sequelize");
 
+
+
 const sequelize = new Sequelize({
     dialect: "sqlite",
     storage: "../backend/database.sqlite"
@@ -38,53 +40,106 @@ AcademicAdvisor.belongsTo(Lecturer, { through: "Academic Advisor" });
 CourseLeader.belongsTo(Lecturer, { through: "Course Leader" });
 
 //Course Leader to Course
-Course.hasOne(CourseLeader);
-CourseLeader.hasOne(Course);
+Course.belongsTo(CourseLeader);
+CourseLeader.belongsTo(Course);
 
 //Course to Module, one course contains many modules
-Course.hasMany(Module);
-Module.hasMany(Course);
+const CourseModules = sequelize.define('Course Modules', {}, { timestamps: false });
+module.exports = CourseModules;
+Course.belongsToMany(Module, { through: CourseModules });
+Module.belongsToMany(Course, { through: CourseModules });
 
 //Many groups in one module, one module per group
 Module.hasMany(Group);
-Group.hasOne(Module);
+Group.belongsTo(Module);
 
-//Many users in 
-Module.belongsToMany(User, { through: "User Modules" });
-User.belongsToMany(Module, { through: "User Modules" });
+//Many users in a module
+const UserModules = sequelize.define('User Modules', {}, { timestamps: false });
+module.exports = UserModules;
+Module.belongsToMany(User, { through: UserModules });
+User.belongsToMany(Module, { through: UserModules });
 
 //Array of Lessons in a group
-Lesson.hasOne(Group);
+Lesson.belongsTo(Group);
 Group.hasMany(Lesson);
 
 //Array of groups in User, array of users in each group
-Group.belongsToMany(Student, { through: "Student Groups" });
-Student.belongsToMany(Group, { through: "Student Groups" });
+const StudentGroups = sequelize.define('Student Groups', {}, { timestamps: false });
+module.exports = StudentGroups;
+Group.belongsToMany(Student, { through: StudentGroups });
+Student.belongsToMany(Group, { through: StudentGroups });
 
 //Lecturers in Groups and vice versa
-Group.belongsToMany(Lecturer, { through: "Lecturer Groups" });
-Lecturer.belongsToMany(Group, { through: "Lecturer Groups" });
+const LecturerGroups = sequelize.define('Lecturer Groups', {}, { timestamps: false });
+module.exports = LecturerGroups;
+Group.belongsToMany(Lecturer, { through: LecturerGroups });
+Lecturer.belongsToMany(Group, { through: LecturerGroups });
 
 //Attendance Table
 Lesson.belongsToMany(Student, { through: Attendance });
 Student.belongsToMany(Lesson, { through: Attendance });
 
 //Students to Academic Advisors
-Student.hasOne(AcademicAdvisor);
-AcademicAdvisor.hasMany(Student);
+Student.belongsTo(AcademicAdvisor);
+const AcademicAdvisees = sequelize.define('Academic Advisees', {}, { timestamps: false });
+module.exports = AcademicAdvisees;
+AcademicAdvisor.belongsToMany(Student, { through: AcademicAdvisees });
+
+function createDummyData() {
+    // student1.save();
+    const student1 = Student.build({ User: { firstName: "Mazen", lastName: "Omar", username: "mo190201", password: "test123" } }, { include: [User] });
+    student1.save();
+
+    const lecturer1 = Lecturer.build({
+        User: { firstName: "Nathan", lastName: "Blakemore", username: "nb2948382", password: "test123" }
+    }, { include: [User] });
+    // lecturer1.save();
+    // const advisorUser = User.build({ firstName: "Nathan", lastName: "Blakemore", username: "nb2948382", password: "test123" })
+    // advisorUser.save();
+    const advisor1 = AcademicAdvisor.build({
+        Lecturer: {
+            User: { firstName: "Nathan", lastName: "Blakemore", username: "nb2948382", password: "test123" }
+        }
+    }, { include: [User, Lecturer] });
+    // const advisor1 = AcademicAdvisor.build({
+    //     Lecturer: {
+    //         User: { firstName: "Nathan", lastName: "Blakemore", username: "nb2948382", password: "test123" }
+    //     },
+    //     { include: [User] }
+    // }, { include: [Lecturer] });
+    advisor1.save();
+
+    // user2 = User.findOne({ where: { firstName: "Nathan" } });
+    // userID = user2.id;
+
+    // const lecturer2 = Lecturer.findOne({ where: { id: userID } });
+
+    // const advisor1 = AcademicAdvisor.build();
+    // advisor1.save();
+
+    // AcademicAdvisor.upsert({
+    //     id: 2,
+    //     LecturerId: lecturer2.id,
+    // })
+
+}
+
+//createDummyData();
+
+//const sameAdvisor = AcademicAdvisor.findByPk(1);
 
 
-// const user1 = User.build({ firstName: "Mazen", lastName: "Omar", username: "mo190201", password: "test123" });
-// user1.save();
-// student1.save();
-// const student1 = Student.build({ User: { firstName: "Mazen", lastName: "Omar", username: "mo190201", password: "test123" } }, { include: [User] });
-// student1.save();
-// Student.upsert({
-//     id: 3,
-//     AcademicAdvisorId: 3,
-// });
+//createDummyData();
+
+
 // //student1.set({ AcademicAdvisor: {} })
 
+
+async function clearTables() {
+    await sequelize.drop();
+}
+
+//clearTables();
 
 sequelize
     .sync()
